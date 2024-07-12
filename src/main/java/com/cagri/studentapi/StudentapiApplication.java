e4575f842fc4443ad99e2c9b4252a5c06fd905a5
C. Çağrı YILMAZ <cuneytcagriyilmaz@hotmail.com>
2024-07-12 13:48:26 +0300

    first commit

---
+++ src/main/java/com/cagri/studentapi/StudentapiApplication.java
@@ -0,0 +1,14 @@
+package com.cagri.studentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentapiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("deneme");
+		SpringApplication.run(StudentapiApplication.class, args);
+	}
+
+}
